36b8c89a68244b5906333facfa174f63583e104d
Jake Astles <jakeastles13@gmail.com>
2025-02-13 08:58:51 +0000

    Made it so the player has a fancy masked damage animation and fixed the background issues

--- Assets/Scripts/TopDownCharacterController.cs
+++ Assets/Scripts/TopDownCharacterController.cs
@@ -1,6 +1,7 @@
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections;
 
 /// <summary>
 /// A class to control the top down character.
@@ -15,6 +16,7 @@ public class TopDownCharacterController : MonoBehaviour
     private InputAction m_moveAction;
     private InputAction m_attackAction;
     private InputAction m_rollAction;
+    private InputAction m_lookAction;
 
     //The components that we need to edit to make the player move smoothly.
     private Animator m_animator;
@@ -22,13 +24,14 @@ public class TopDownCharacterController : MonoBehaviour
     
     //The direction that the player is moving in.
     private Vector2 m_playerDirection;
-   
 
-    [Header("Movement parameters")]
+    [Header("Movement Parameters")]
     //The speed at which the player moves
     [SerializeField] private float m_playerSpeed = 200f;
     //The maximum speed the player can move
     [SerializeField] private float m_playerMaxSpeed = 1000f;
+    //How fast you can roll - a method of indirectly accessing the rollTimeout variable
+    [SerializeField] float m_rollRate;
 
     #endregion
 
@@ -36,17 +39,15 @@ public class TopDownCharacterController : MonoBehaviour
     /// When the script first initialises this gets called.
     /// Use this for grabbing components and setting up input bindings.
     /// </summary>
-    [Header("Projectile parameters")]
+    [Header("Projectile Parameters")]
     //Reference to the game obj
     [SerializeField] GameObject m_projectilePrefab;
-    //Wgere the projectile will be spawned from
+    //Where the projectile will be spawned from
     [SerializeField] Transform m_firePoint;
     //How fast our projectile will travel
     [SerializeField] float m_projectileSpeed;
     //How fast the projectiles will fire
     [SerializeField] float m_fireRate;
-    //How fast you can roll - a method of indirectly accessing the rollTimeout variable
-    [SerializeField] float m_rollRate;
 
     [Header("Lighting Parameters")]
     [SerializeField] private Transform m_torchLight;
@@ -56,12 +57,28 @@ public class TopDownCharacterController : MonoBehaviour
     private float m_fireTimeout = 0;
     private Vector2 m_LastDirection;
     private Quaternion m_targetRotation;
+    private bool m_move;
+    private bool m_isRolling;
+
+    [Header("Mask Parameters")]
+    //The mask applied when the player receives damage
+    [SerializeField] private GameObject m_maskCore;
+    [SerializeField] private GameObject m_maskOuter;
+    //The period of time the mask would be visible for
+    [SerializeField] private float m_maskDuration = 1f;
+    //The minimum and maximum sizes of the mask when growing
+    [SerializeField] private float m_maskMinSize = 0.5f;
+    [SerializeField] private float m_maskMaxSize = 0.3f;
+    [SerializeField] private int m_maskStabCount = 3;
+
+
     private void Awake()
     {
         //bind movement inputs to variables
         m_moveAction = InputSystem.actions.FindAction("Move");
         m_attackAction = InputSystem.actions.FindAction("Attack");
         m_rollAction = InputSystem.actions.FindAction("Jump");
+        m_lookAction = InputSystem.actions.FindAction("Interact");
 
         //get components from Character game object so that we can use them later.
         m_animator = GetComponent<Animator>();
@@ -73,7 +90,9 @@ public class TopDownCharacterController : MonoBehaviour
     /// </summary>
     void Start()
     {
-        
+        m_move = false;
+        m_maskCore.gameObject.SetActive(false);//Hides the masks on start
+        m_maskOuter.gameObject.SetActive(false);
     }
 
     /// <summary>
@@ -84,7 +103,11 @@ public class TopDownCharacterController : MonoBehaviour
     {
         //clamp the speed to the maximum speed for if the speed has been changed in code.
         float speed = m_playerSpeed > m_playerMaxSpeed ? m_playerMaxSpeed : m_playerSpeed;
-        
+
+        if (!m_move)//only move if your not holding right click
+        {
+            speed = 0.0f;
+        }
         //apply the movement to the character using the clamped speed value.
         m_rigidbody.linearVelocity = m_playerDirection * (speed * Time.fixedDeltaTime);
     }
@@ -97,104 +120,99 @@ public class TopDownCharacterController : MonoBehaviour
     void Update()
     {
         // store any movement inputs into m_playerDirection - this will be used in FixedUpdate to move the player.
-        m_playerDirection = m_moveAction.ReadValue<Vector2>();
-        
-        // ~~ handle animator ~~
-        // Update the animator speed to ensure that we revert to idle if the player doesn't move.
-        m_animator.SetFloat("Speed", m_playerDirection.magnitude);
-        
-        // If there is movement, set the directional values to ensure the character is facing the way they are moving.
-        if (m_playerDirection.magnitude > 0)
+        if (m_lookAction.IsPressed())
         {
-            m_animator.SetFloat("Horizontal", m_playerDirection.x);
-            m_animator.SetFloat("Vertical", m_playerDirection.y);
-
-            m_LastDirection = m_playerDirection;//Sets the last facing direction of the player
+            Vector3 m_mousePosition = Input.mousePosition;//The mouse position
+            Vector3 mousePointOnScreen = Camera.main.ScreenToWorldPoint(m_mousePosition);
+            m_playerDirection = (mousePointOnScreen - m_torchLight.position).normalized;
+            m_move = false;
         }
+        else
+        {
+            m_playerDirection = m_moveAction.ReadValue<Vector2>();
+            m_move = true;
+        }
+            // ~~ handle animator ~~
+            // Update the animator speed to ensure that we revert to idle if the player doesn't move.
+            m_animator.SetFloat("Speed", m_playerDirection.magnitude);
+
+        if (m_playerDirection != m_LastDirection)
+        {
+            // If there is movement, set the directional values to ensure the character is facing the way they are moving.
+            if (m_playerDirection.magnitude > 0)
+            {
+                m_animator.SetFloat("Horizontal", m_playerDirection.x);
+                m_animator.SetFloat("Vertical", m_playerDirection.y);
+
+                m_LastDirection = m_playerDirection;//Sets the last facing direction of the player
+            }
+            //Converts the x and y of m_playerDirection to polar coordinates, i.e. an angle
+            float angle = Mathf.Atan2(m_playerDirection.y, m_playerDirection.x) * Mathf.Rad2Deg;
+            //Sets the rotation to this angle on Z and doesn't update the angle unless there is a new valid input
+            m_targetRotation = m_playerDirection.magnitude < 0.01f ? m_targetRotation : Quaternion.Euler(0, 0, angle - 90);
+        }
+            //smoothly rotates the light using the player's orientation
+            m_torchLight.rotation = Quaternion.Slerp(m_torchLight.rotation, m_targetRotation, m_rotationSpeed * Time.deltaTime);
 
         // check if an attack has been triggered.
         if (m_attackAction.IsPressed() && Time.time > m_fireTimeout)//gives the bullets a delay between firing
         {
-            // just log that an attack has been registered for now
-            // we will look at how to do this in future sessions.
-            Debug.Log("Attack!");
-
             m_fireTimeout = Time.time + m_fireRate;//adding the fire rate increases fire timeout more and more this which means the if statement has to wait for the delay
-            //Debug.Log(Time.time);//increasing it forever is okay because it is being compared to time which also always increases
+            //increasing m_fireTimeout forever is okay because it is being compared to time which also always increases
             Fire();
+            TakeDamage();
         }
-
-        if (m_rollAction.IsPressed() && Time.time > m_rollTimeout)
+        if (m_rollAction.IsPressed() && Time.time > m_rollTimeout && !m_isRolling)
         {
+            m_isRolling = true; // Set rolling state
+            m_playerSpeed = m_playerSpeed * 2;
             m_animator.SetTrigger("Rolling");
-            m_rollTimeout = Time.time + m_rollRate;
+            m_rollTimeout = Time.time + m_rollRate; // Set cooldown timer
             //Time.time + m_rollRate essentially just saying if the current time is equal to the time 2 seconds into the future
             //saved to the fireRate variable so it isn't constantly increasing
+            // Reset after roll duration
+            //StartCoroutine(EndRoll(m_rollRate));
         }
+    }
 
-        if (m_playerDirection != m_LastDirection) 
-        {
-            //Converts the x and y of m_playerDirection to polar coordinates, i.e. an angle
-            float angle = Mathf.Atan2(m_playerDirection.y, m_playerDirection.x) * Mathf.Rad2Deg;
-            m_targetRotation = Quaternion.Euler(0, 0, angle - 90);
-            //Sets the rotation to this angle on Z
-            m_LastDirection = m_playerDirection;
-            //m_torchLight.rotation = Quaternion.Euler(0, 0, angle - 90);
-            m_torchLight.rotation = Quaternion.RotateTowards(m_torchLight.rotation, m_targetRotation, m_rotationSpeed * Time.deltaTime);
-        }
-}
     void Fire()//spawns a bullet from the location of the firepoint at the rotation of identity (null)
     {
-        // Vector3 mousePointOnScreen = Camera.main.ScreenToWorldPoint(mousePos);
-        Vector2 fireDirection = m_LastDirection;
-        Quaternion bulletRotation = Quaternion.identity;
-
-        //left right and idle directions
-        if (fireDirection == Vector2.zero)
-        {
-            fireDirection = Vector2.down;
-        }
-        else if (fireDirection == Vector2.left)
-        {
-            bulletRotation = Quaternion.Euler(0, 0, 90f);//if the direction faced is left rotate the bullet 90 degrees left
-        }
-        else if (fireDirection == Vector2.right)
-        {
-            bulletRotation = Quaternion.Euler(0, 0, 270f);//the same for facing right but in reverse
-        }
-
-        //NE SE SW NW diagonal directions
-        // Handle quadrant-based rotations
-        if (fireDirection == new Vector2(-0.71f, 0.71f))//NW
-        {
-            // Up-Left: 135 degrees
-            bulletRotation = Quaternion.Euler(0, 0, 135f);
-            Debug.Log("UP Left");
-        }
-        else if (fireDirection == new Vector2(0.71f, 0.71f))//NE
-        {
-            // Up-Right: 45 degrees
-            bulletRotation = Quaternion.Euler(0, 0, 45f);
-        }
-        else if (fireDirection == new Vector2(-0.71f, -0.71f))//SW
-        {
-            // Down-Left: 225 degrees
-            bulletRotation = Quaternion.Euler(0, 0, 225f);
-        }
-        else if (fireDirection == new Vector2(0.71f, -0.71f))//SE
-        {
-            // Down-Right: 315 degrees (or 0 to 45 for covering the full circle)
-            bulletRotation = Quaternion.Euler(0, 0, 315f);
-        }
-        Debug.Log(fireDirection);
-        Debug.Log((Vector2.left / 2) - new Vector2(0.21f,0.21f));
-
-        GameObject projectileToSpawn = Instantiate(m_projectilePrefab, m_firePoint.position, bulletRotation);//spawns a projectile using the prefab-object, firepoint and bullet rotation variables
+        GameObject projectileToSpawn = Instantiate(m_projectilePrefab, m_firePoint.position, m_targetRotation);//spawns a projectile using the prefab-object, firepoint and target rotation variables
         if (projectileToSpawn.GetComponent<Rigidbody2D>() != null)
         {
-            projectileToSpawn.GetComponent<Rigidbody2D>().AddForce(fireDirection.normalized * m_projectileSpeed, ForceMode2D.Impulse); //adds force to the object's y axis in relation to its speed
-            //the forcemode2d, type impulse is instantaneous as oppose to type force which is continuous
+            projectileToSpawn.GetComponent<Rigidbody2D>().AddForce(m_LastDirection.normalized * m_projectileSpeed, ForceMode2D.Impulse);//adds force to the object's y axis in relation to its speed
+            //the forcemode2D, type impulse is instantaneous as oppose to type force which is continuous
             //a gun would apply force once to a bullet so impulse seems more relevant on this occasion
+        }
+    }
+
+    public void TakeDamage()
+    {
+        int i = 0;
+        StartCoroutine(ShowDamageMask(i));
+    }
+
+    private IEnumerator ShowDamageMask(int i)
+    {
+        m_maskCore.gameObject.SetActive(true);//Shows the mask
+        m_maskOuter.gameObject.SetActive(true);
+
+        LeanTween.scale(m_maskOuter, Vector3.one * (m_maskMinSize * 4), m_maskDuration / 4f)//Grow animation
+            .setEase(LeanTweenType.easeOutQuad)
+            .setOnComplete(() =>
+            {
+                LeanTween.scale(m_maskOuter, Vector3.one * (m_maskMaxSize * 2), m_maskDuration / 4f)//Shrink animation
+                    .setEase(LeanTweenType.easeInQuad);
+            });
+
+        yield return new WaitForSeconds(m_maskDuration);
+        
+        m_maskCore.gameObject.SetActive(false);//Hides the mask after the duration
+        m_maskOuter.gameObject.SetActive(false);
+
+        if (++i < m_maskStabCount)
+        {
+            StartCoroutine(ShowDamageMask(i));
         }
     }
 }
